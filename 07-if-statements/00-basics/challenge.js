e0bf8a28b5e6b249dde9ad27ed67e69a54564210
Ricardo Vazquez <2ricardo.vazquez@gmail.com>
2014-11-17 01:31:28 +0000

    Fixing if/else specs

--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -1,5 +1,27 @@
-module.exports.favoriteNumber = undefined;
+module.exports.favoriteNumber = function favoriteNumber(fav, guess){
+  if (guess > fav) {
+    return "Too high";
+  }
+  else if (guess < fav) {
+    return "Too low";
+  }
+  else if (guess === fav) {
+    return "You got it!";
+  }
+};
 
-module.exports.checkLock = undefined;
+module.exports.checkLock = function checkLock(a, b, c, d){
+  if ((a == 3 || a == 5 || a == 7) && b == 2 && (c >= 5 && c <= 100) && (d < 9 || d > 20)){
+    return "correct";
+  } else {
+    return "incorrect";
+  }
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function canIGet(item, money){
+  if ((item === 'MacBook Air' && money >= 999) || (item === 'MacBook Pro' && money >= 1299) || (item === 'Mac Pro' && money >= 2499) || (item === 'Apple Sticker' && money >= 1)){
+    return true;
+  } else {
+    return false;
+  }
+};
