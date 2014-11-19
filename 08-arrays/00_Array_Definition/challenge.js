9b316b2a370050744f395fc277079b795cfa77ce
Ricardo Vazquez <2ricardo.vazquez@gmail.com>
2014-11-19 15:24:34 +0000

    Fixing array specs

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,14 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function newArray(w, x, y, z){
+  var array = [w, x, y, z];
+  return array;
+};
 
-module.exports.firstAndLast = undefined;
+module.exports.firstAndLast = function firstAndLast(array){
+  var lastElementIndex = array.length - 1,
+      firstValue = array[0],
+      lastValue = array[lastElementIndex];
+      nArray = [firstValue, lastValue];
+
+  return nArray;
+
+}
