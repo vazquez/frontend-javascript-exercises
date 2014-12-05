0c0452db2fb891312219154274e3aa9eb13cf805
Ricardo Vazquez <2ricardo.vazquez@gmail.com>
2014-12-05 20:04:41 +0000

    Fxing specs for objects

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,30 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents){
+  var obj = {
+    title: courseTitle,
+    duration: courseDuration,
+    students: courseStudents
+  };
+  return obj;
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue){
+  if (!object.hasOwnProperty(newProp)) {
+    object[newProp] = newValue;
+  }
+  return object;
+}
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter){
+  var string = "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender;
+  return string;
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money){
+  var obj = {
+    "MacBook Air": 999,
+    "MacBook Pro": 1299,
+    "Mac Pro": 2499,
+    "Apple Sticker": 1
+  }
+  return obj[item] <= money;
+ };
