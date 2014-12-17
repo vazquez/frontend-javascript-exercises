a3875dad91773b471ecfc3b28645b3085839094b
Ricardo Vazquez <2ricardo.vazquez@gmail.com>
2014-12-17 22:16:27 +0000

    Fixing specs for looping over arrays

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,24 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(array){
+  var result = 0;
+  var index = 0;
+  for (var i = 0; i < array.length; i++) {
+    result += array[index];
+    index++;
+  }
+  return result;
+};
 
-module.exports.splitAndLowerCaseString = undefined;
+module.exports.splitAndLowerCaseString = function(input){
+  var finalString = input.toLowerCase().split(",");
+  return finalString;
+};
 
-module.exports.addIndex = undefined;
+module.exports.addIndex = function(array){
+  var newArray = [];
+  var index = 0;
+  for (var i = 0; i < array.length; i++) {
+    newArray.push(index + " is " + array[index]);
+    index++;
+  }
+  return newArray;
+};
